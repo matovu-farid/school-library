c36e9e976bdf88295da58b5fbafa7e568e93b3a1
matovu-farid <matovu90@gmail.com>
2022-04-06 15:29:37 +0300

    Fix: linting errors

--- src/person.rb
+++ src/person.rb
@@ -2,7 +2,7 @@ require_relative 'nameable'
 require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
 class Person < Nameable
-  attr_accessor :name, :age , :rentals
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
