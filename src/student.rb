f943727e21fe98b976b12dd8ea1775063c8180f1
matovu-farid <matovu90@gmail.com>
2022-04-06 15:03:27 +0300

    Make the classroom public

--- src/student.rb
+++ src/student.rb
@@ -1,5 +1,7 @@
 require_relative 'person'
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom, age, name, parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
