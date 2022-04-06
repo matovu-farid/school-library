b5b3f09025bf353a00e24e86dd645c11928cc5df
matovu-farid <matovu90@gmail.com>
2022-04-06 15:28:32 +0300

    Add the rentals to person

--- src/person.rb
+++ src/person.rb
@@ -2,7 +2,7 @@ require_relative 'nameable'
 require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :age , :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -11,6 +11,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def correct_name
