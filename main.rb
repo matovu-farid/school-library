272de8559375d7eb33a493a4274c68cac9212a42
matovu-farid <matovu90@gmail.com>
2022-04-07 19:57:04 +0300

    Add dome spacing

--- main.rb
+++ main.rb
@@ -11,16 +11,22 @@ class Main
     case @choice
     when '1'
       @app.list_books
+      puts
     when '2'
       @app.list_people
+      puts
     when '3'
       @app.create_person
+      puts
     when '4'
       @app.create_book
+      puts
     when '5'
       @app.create_rental
+      puts
     when '6'
       @app.list_rentals
+      puts
     end
   end
 
