06a463239ff56309518ebb97ef7c7424084fe2bb
matovu-farid <matovu90@gmail.com>
2022-04-11 15:49:48 +0300

    Refactor main.rb

--- main.rb
+++ main.rb
@@ -1,47 +1,14 @@
 require_relative 'src/app'
-
-class Main
-  def initialize
-    @choice = ''
-    @default = Default.new
-    @app = App.new
-  end
-
-  def case_statement
-    case @choice
-    when '1'
-      @app.list_books
-      puts
-    when '2'
-      @app.list_people
-      puts
-    when '3'
-      @app.create_person
-      puts
-    when '4'
-      @app.create_book
-      puts
-    when '5'
-      @app.create_rental
-      puts
-    when '6'
-      @app.list_rentals
-      puts
-    end
-  end
-
-  def run
-    until @choice == '7'
-      @choice = @default.choose
-      case_statement
-    end
-  end
-end
+require_relative './src/case_statement'
+require_relative './src/display_classes/default'
 
 def main
-  Main.new.run
-rescue StandardError
-  puts 'Wrong value entered'
+  choice = ''
+  app = App.new
+  until choice == '7'
+    choice = Default.new.choose
+    CaseStatement.new(choice, app).run
+  end
 end
 
 main
