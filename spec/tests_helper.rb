d0c7958448444e09cef39f989115ee3ed1aa6fec
matovu-farid <matovu90@gmail.com>
2022-04-13 17:09:09 +0300

    Create test helper

---
+++ spec/tests_helper.rb
@@ -0,0 +1,7 @@
+require_relative '../src/book'
+require_relative '../src/classroom'
+require_relative '../src/rental'
+require_relative '../src/person'
+require_relative '../src/student'
+require_relative '../src/teacher'
+require_relative '../src/trimmer_decorator'
