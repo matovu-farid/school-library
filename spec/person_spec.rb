786d326c36dfd0e0a19169f51957471da593e59c
matovu-farid <matovu90@gmail.com>
2022-04-14 10:17:49 +0300

    can use services to the person

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -22,4 +22,24 @@ describe Person do
     rentals = @person.rentals.count
     expect(rentals).to eq 1
   end
+  it 'cannot use services if student is not of age' do
+    person = Person.new(0, 'Name', id: 300)
+    boolean = person.can_use_services?
+    expect(boolean).to be false
+  end
+  it 'can use services if student is not of age' do
+    person = Person.new(18, 'Name', id: 300)
+    boolean = person.can_use_services?
+    expect(boolean).to be true
+  end
+  it 'cannot use services if parents_permission is false' do
+    person = Person.new(18, 'Name', id: 300, parent_permission: false)
+    boolean = person.can_use_services?
+    expect(boolean).to be false
+  end
+  it 'can use services if student is not of age' do
+    person = Person.new(18, 'Name', id: 300, parent_permission: true)
+    boolean = person.can_use_services?
+    expect(boolean).to be true
+  end
 end
